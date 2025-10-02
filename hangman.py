ae0b6998a115860243810616912babff490d9a81
sagar-202 <sagarpatgar@gmail.com>
2025-10-02 10:26:29 +0530

    new
    changes made

--- hangman.py
+++ hangman.py
@@ -9,10 +9,21 @@ for position in range(word_length):
     placeholder += "_"
 print(placeholder)
 
-guess = input("Guess a letter: ").lower()
+game_over = False
+correct_letter = []
+while not game_over:
+    guess = input("Guess a letter: ").lower()
+    display = ""
 
-for letter in choosen_word:
-    if letter == guess:
-        print("Right")
-    else:
-        print("Wrong")
+    for letter in choosen_word:
+        if letter == guess:
+            display += letter
+            correct_letter.append(guess)
+        elif letter in correct_letter:
+            display += letter
+        else:
+            display += "_"
+    print(display)
+    if "_" not in display:
+        game_over = True
+        print("You win")
